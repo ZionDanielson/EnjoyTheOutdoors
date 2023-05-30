fa7530963eb9ff248984b6bd84d3d53aad80b600
Zion Danielson <zionsdocs@gmail.com>
2023-05-30 08:31:52 -0400

    index.html

--- scripts/mountains_info.js
+++ scripts/mountains_info.js
@@ -14,6 +14,19 @@ let mountainInfoCard = document.getElementById("mountainInfoCard"); // Mountain 
 
 let infoCardContainer = document.getElementById("infoCardContainer"); // Container for the Mountain info cards
 
+
+const mountainImage = document.getElementById("mountainImage");
+
+const mountainName = document.getElementById("mountainName");
+
+const mountainElevation = document.getElementById("mountainElevation");
+
+const mountainEffort = document.getElementById("mountainEffort");
+
+const mountainDesc = document.getElementById("mountainDesc");
+
+const mountainCoords = document.getElementById("mountainCoords");
+
 /*_____________________________________________________________________________________________________________________________________*/
 
 
@@ -78,8 +91,20 @@ function hideMountainInfoCard() {
 
 } 
 
-/*_____________________________________________________________________________________________________________________________________
+/*_____________________________________________________________________________________________________________________________________*/
 
+// When page loads...                     ******** HIDE MOUNTAIN INFO CARDS ********
+
+
+function showMountainInfoCard() {
+
+    mountainInfoCard.style.display = "block";  
+          
+   console.log(`Mountain Info Card is Shown`);
+
+} 
+
+/*_____________________________________________________________________________________________________________________________________
 
 
 
@@ -89,19 +114,69 @@ function hideMountainInfoCard() {
 
 _____________________________________________________________________________________________________________________________________*/
 
-// When Mountain Dropdown is Changed...      ******** DISPLAY MOUNTAIN INFO CARD ********
+// // When Mountain Dropdown is Changed...      ******** DISPLAY MOUNTAIN INFO CARD ********
+
+// function whenMountainIsChanged() {
+
+
+// // let selectedMountain = selectedMountain.value; // Converts selection => value
+
+// // if (selectedMountain == "") { // if there is no value show no card
+// //     hideMountainInfoCard();
+
+// // }
+// // else {
+
+//     // let selectedMountain = getActivityById(selectedMountain.value);
+
+//     mountainImage.innerHTML = selectedMountain.img;
+//     mountainName.innerHTML = selectedMountain.name;
+//     mountainElevation.innerHTML = selectedMountain.elevation;
+//     mountainEffort.innerHTML = selectedMountain.effort;
+//     mountainDesc.innerHTML = selectedMountain.desc;
+//     mountainCoords.innerHTML = selectedMountain.coords;
+
+//     showMountainInfoCard();
+// }
 
 function whenMountainIsChanged() {
-
-
-    const selectedMountain = theMountainsDropdown.value; // Converts selection => value
-
-    console.log(selectedMountain);
-
-        if (theMountainsDropdown.value !== "") {  // If a value is present...
-
-            mountainInfoCard()  // initiate this function
-
+    
+    
+    let selectedMountain = theMountainsDropdown.value; // Retrieve the selected mountain value from the dropdown
+    if (selectedMountain) {
+      // Check if a mountain is selected
+      // Retrieve the mountain information based on the selected value
+      let mountainInfo = document.getElementById(selectedMountain);
+      
+        // Update the info card elements with the retrieved mountain information
+        //mountainImage.innerHTML = mountainInfo.img;
+        mountainName.innerHTML = selectedMountain.name;
+        mountainElevation.innerHTML = selectedMountain.elevation;
+        mountainEffort.innerHTML = selectedMountain.effort;
+        mountainDesc.innerHTML = selectedMountain.desc;
+        mountainCoords.innerHTML = selectedMountain.coords;
+  
+        showMountainInfoCard();
+  }
 }
 
-}
+  
+
+
+/*_____________________________________________________________________________________________________________________________________*/
+
+
+//  // When initiated...                     ******** MOUNTAIN INFO CARD ********
+
+
+//  function theMountainInfoCard() {
+    
+  
+//     showmountainInfoCard(selectedMountain); // This function initiates showing the MOUNTAIN info card
+//   }
+  
+            
+//         console.log(`The Mountain Info Card is displayed on the screen`);
+        
+        
+/*_____________________________________________________________________________________________________________________________________*/
